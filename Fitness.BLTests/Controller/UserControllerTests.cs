422c6684948d7725b6cd86cddd166ed219b44d55
Justovich <soulden1337@yandex.ru>
2019-09-02 20:01:07 +0500

    Модульные тесты

---
+++ Fitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SaveTest()
+        { 
+            var userName = Guid.NewGuid().ToString();
+
+            var controller = new UserController(userName);
+
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            var userName = Guid.NewGuid().ToString();
+            var birthdate = DateTime.Now.AddYears(-18);
+            var weight = 90;
+            var height = 190;
+            var gender = "man";
+            var controller = new UserController(userName);
+
+            controller.SetNewUserData(userName, birthdate, weight, height);
+            var controller2 = new UserController(userName);
+
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+            Assert.AreEqual(weight, controller2.CurrentUser.Weight);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
+            Assert.AreEqual(birthdate, controller2.CurrentUser.BirthDate);
+        }
+    }
+}
